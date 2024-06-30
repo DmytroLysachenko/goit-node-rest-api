f669a291ac01b7aefd113c7432167feef4215eaa
Dimka863 <dlysachenko98@gmail.com>
2024-06-30 19:05:49 +0200

    Tested - all ok

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -3,7 +3,8 @@ import HttpError from './HttpError.js';
 const validateBody = (schema) => {
   const func = (req, _, next) => {
     const { error } = schema.validate(req.body);
-    if (error.message === '"value" must have at least 1 key') {
+
+    if (error && error.message === '"value" must have at least 1 key') {
       next(HttpError(400, 'Body must have at least one field'));
     }
     if (error) {
